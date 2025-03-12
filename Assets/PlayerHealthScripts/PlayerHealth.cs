aa4af80e446aafcb6f3213a7bc0116c1a5b5825a
THERMELINK <thermelink01@student.rocvantwente.nl>
2025-03-12 13:27:48 +0100

    feat: added a Boel
    boel = new version of bool

--- Assets/PlayerHealthScripts/PlayerHealth.cs
+++ Assets/PlayerHealthScripts/PlayerHealth.cs
@@ -8,6 +8,8 @@ public class PlayerHealth : MonoBehaviour
     public float playerHealth;
     public float maxPossibleHealth = 100.0f;
 
+    bool testboel = true;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
