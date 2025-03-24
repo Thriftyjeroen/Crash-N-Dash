77117a1c0a944737a5861a4211d8fc59979894bd
Gido <130654343+Egg321123@users.noreply.github.com>
2025-03-24 09:33:23 +0100

    feat: updated gido testing scene

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -6,7 +6,6 @@ public class CheckPoint : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log($"Collision with: {other.gameObject.name}");
-        //health test script should be replaced with a player manager
         if (other.TryGetComponent<Player>(out Player player))
         {
             checkPointManager.PassCheckPoint(this);
--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    //CheckList
-    //Spawn Objects on random position
-    //Only spawn objects on track
-    //Spawn an object every lap
+    //CheckList    
     //check if there is an item avaliable in the pool
+
     [SerializeField] private GameObject obstacle;
     Vector3 spawnPosition;
 
@@ -24,15 +22,15 @@ public class ObstacleSpawner : MonoBehaviour
 
     public void OnLap()
     {
-        // Generate a random position within the range of the map
-        spawnPosition = new Vector3(Random.Range(-10f, 10f), Random.Range(-5f, 5f), 0);
+        //// Generate a random position within the range of the map
+        //spawnPosition = new Vector3(Random.Range(-10f, 10f), Random.Range(-5f, 5f), 0);
 
-        // Check if the position is on a track using Physics2D.OverlapPoint
+        //// Check if the position is on a track using Physics2D.OverlapPoint
         //Collider2D hitCollider = Physics2D.OverlapPoint(spawnPosition);
         //if (hitCollider != null && hitCollider.CompareTag("Track"))
         //{
-            // Instantiate the obstacle at a valid position
-            Instantiate(obstacle, spawnPosition, Quaternion.identity);
+        //    // Instantiate the obstacle at a valid position
+           Instantiate(obstacle, spawnPosition, Quaternion.identity);
         //}
         //else
         //{
