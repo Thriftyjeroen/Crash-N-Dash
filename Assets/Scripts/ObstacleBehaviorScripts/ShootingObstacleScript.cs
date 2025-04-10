26a5d9c0f9651e01277d7795b8b5b20e5d746f74
THERMELINK <thermelink01@student.rocvantwente.nl>
2025-04-10 10:35:56 +0200

    fix: fixed flamethrower bug
    fixed flamethrower bug and made code more readable with removed lines and added comments

--- Assets/Scripts/ObstacleBehaviorScripts/ShootingObstacleScript.cs
+++ Assets/Scripts/ObstacleBehaviorScripts/ShootingObstacleScript.cs
@@ -11,19 +11,20 @@ public class ObstacleScript : MonoBehaviour
 {
     public GameObject[] players;
     public GameObject healthManager;
-    public GameObject parentObstacle;
+    public GameObject turretGameObject;
     public bool allowedToShoot = true;
-    public bool isFlameThrowerShooting = false;
     [SerializeField] GameObject bulletPrefab;
+    GameObject flame = null;
     string Playertag = "Player";
-
-    //  [SerializeField]GameObject testPlayerObject;
-
     float bulletSpeed = 100;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //gets the gameobject where this script is attached to
+        turretGameObject = gameObject;
+
+        //find every player (a player is a gameobject with the player tag)
         players = GameObject.FindGameObjectsWithTag(Playertag);
         try
         {
@@ -31,24 +32,31 @@ public class ObstacleScript : MonoBehaviour
         }
         catch
         {
+            //if not found print this string
             print("cannot find object with playerhealtcheck script attached to it");
         }
-        parentObstacle = this.gameObject;
-        if(parentObstacle.name.Contains("flamethrower"))
+
+        //if the gameObject name is flamethrower it turns off the flame
+        if(turretGameObject.name.Contains("flamethrower"))
         {
-            parentObstacle.transform.GetChild(0).gameObject.SetActive(isFlameThrowerShooting);
+            flame = turretGameObject.transform.GetChild(0).gameObject;
+            flame.SetActive(false);
         }
     }
 
     // Update is called once per frame
+    //in the update it chooses an action to perform based on the name of the gameobject
     void Update()
     {
         if (allowedToShoot == true)
         {
-            float distance = Vector3.Distance(parentObstacle.transform.position, findClosestPlayer(players, parentObstacle));
+            //finds the distance between this turret and the closest player
+            float distance = Vector3.Distance(turretGameObject.transform.position, findClosestPlayer(players, turretGameObject));
+
+            //if the distance is less than 5, it can shoot the bullet
             if (distance < 5)
             {
-                switch (parentObstacle.name)
+                switch (turretGameObject.name)
                 {
                     // with the "when" keyword another condition can be added, basically an if statement kinda
                     case string name when name.Contains("TurretGun"):
@@ -60,7 +68,7 @@ public class ObstacleScript : MonoBehaviour
                     case string name when name.Contains("flamethrower"):
                         if (distance < 3)
                         {
-                            shootFlameThrower(3);
+                            StartCoroutine(shootFlames(3));
                         }
                         break;
                     case string name when name.Contains("lazer"):
@@ -82,58 +90,75 @@ public class ObstacleScript : MonoBehaviour
 
         }
     }
+
+    /// <summary>
+    /// method shoots 1 bullet to the exact position of the player
+    /// </summary>
     IEnumerator shootNormalBullet(float waitForSec)
     {
-        StartCoroutine(InstantiateBullet(parentObstacle.transform.position, findClosestPlayer(players, parentObstacle)));
+        StartCoroutine(InstantiateBullet(turretGameObject.transform.position, findClosestPlayer(players, turretGameObject)));
         allowedToShoot = false;
         yield return new WaitForSeconds(waitForSec);
         allowedToShoot = true;
     }
+
+    /// <summary>
+    /// method shoots 3 bullets in the general direction of the closest player
+    /// </summary>
     IEnumerator shootShotgunBullet(float waitForSec)
     {
         for (int i = 0; i < 3; i++)
         {
-            StartCoroutine(InstantiateBullet(parentObstacle.transform.position, findClosestPlayer(players, parentObstacle) * i));
+            StartCoroutine(InstantiateBullet(turretGameObject.transform.position, findClosestPlayer(players, turretGameObject) * i));
         }
         allowedToShoot = false;
         yield return new WaitForSeconds(waitForSec);
         allowedToShoot = true;
     }
-    void shootFlameThrower(float waitForSec)
-    {
-        allowedToShoot = false;
-        StartCoroutine(shootFlames(5));
-    }
+
+    /// <summary>
+    /// method starts the flameshooting 
+    /// </summary>
     IEnumerator shootFlames(float waitForSec)
     {
-        isFlameThrowerShooting = true;
-        parentObstacle.transform.GetChild(0).gameObject.SetActive(isFlameThrowerShooting);
+        //bools to keep track if the flamethower is shooting
+        flame.gameObject.SetActive(true);
+        allowedToShoot = false;
         int amountOfFlameThrowerCorrections = 20;
+
+        //tracks where the flamethrower needs to shoot
         for (int i = 0; i < amountOfFlameThrowerCorrections; i++)
         {
-            Vector3 targetDir = (findClosestPlayer(players, parentObstacle) - parentObstacle.transform.position).normalized;
-
-            //idk what this does 
+            //finds where the flamethrower should be shooting to hit the closest player
+            Vector3 targetDir = (findClosestPlayer(players, turretGameObject) - turretGameObject.transform.position).normalized;
             float angleOfZ = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
 
-            Quaternion newRotation = Quaternion.Euler(parentObstacle.transform.rotation.x, parentObstacle.transform.rotation.y, angleOfZ - 90);
-            parentObstacle.transform.rotation = newRotation;
+            //turns the flamethrower to the calculated angle
+            Quaternion newRotation = Quaternion.Euler(turretGameObject.transform.rotation.x, turretGameObject.transform.rotation.y, angleOfZ - 90);
+            turretGameObject.transform.rotation = newRotation;
+
+            //wait for amount of secs / amountOfCorrections
             yield return new WaitForSeconds(waitForSec / amountOfFlameThrowerCorrections);
         }
-        isFlameThrowerShooting = false;
-        parentObstacle.transform.GetChild(0).gameObject.SetActive(isFlameThrowerShooting);
+        //sets the flame to not active
+        flame.gameObject.SetActive(false);
 
-        //timer until flamethrower can shoot again
+        //waits for secs before it is allowed to shoots
         yield return new WaitForSeconds(waitForSec);
         allowedToShoot = true;
     }
 
+
+
+    /// <summary>
+    /// method instantiates a bullet, after no hit it deletes itself
+    /// </summary>
     IEnumerator InstantiateBullet(Vector3 thisObstaclePosition, Vector3 closestPlayerPos)
     {
         Vector3 targetDir;
         Vector3 downwardDirection = -transform.up;
         // If it's a DartTrap, shoot straight forward
-        if (parentObstacle.CompareTag("dartTrap"))
+        if (turretGameObject.CompareTag("dartTrap"))
         {
             // Assuming forward is right (x-axis). Adjust based on prefab rotation if needed.
             targetDir = downwardDirection;
@@ -144,15 +169,22 @@ public class ObstacleScript : MonoBehaviour
             targetDir = (closestPlayerPos - thisObstaclePosition).normalized;
         }
 
-        GameObject newBullet = Instantiate(bulletPrefab, parentObstacle.transform.position, Quaternion.identity);
+        //instantiates a new bullet
+        GameObject newBullet = Instantiate(bulletPrefab, turretGameObject.transform.position, Quaternion.identity);
         Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
 
         //add force to direction of player
         rb.AddRelativeForce(targetDir * bulletSpeed);
+
+        //bullet has a lifetime of 4 seconds, if it didnt hit anything it is removed
         yield return new WaitForSeconds(4);
         GameObject.Destroy(newBullet);
     }
 
+
+    /// <summary>
+    /// method finds the closest player position to the gameobject provided 
+    /// </summary>
     Vector3 findClosestPlayer(GameObject[] players, GameObject thisObstacle)
     {
         Vector3 returnthing = new Vector3(0, 0, 0);
