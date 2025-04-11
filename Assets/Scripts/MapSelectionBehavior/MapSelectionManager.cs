3ea83ab51213778991fd1afdbc2453838fdb0fab
THERMELINK <thermelink01@student.rocvantwente.nl>
2025-04-11 13:53:56 +0200

    Fix: random cards are now actually random
    fixed an issue where the cards would not show the map name and it would crash unity(?) also added comments to read it easier

--- Assets/Scripts/MapSelectionBehavior/MapSelectionManager.cs
+++ Assets/Scripts/MapSelectionBehavior/MapSelectionManager.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,8 +12,12 @@ public class MapSelectionManager : MonoBehaviour
     [SerializeField] Button MapSelectTemplate;
     [SerializeField] List<Button> allMapSelectButtons;
     [SerializeField] List<string> AllAvailableMaps;
+    List<string> AlreadyInstantiatedMaps = new List<string>();
     public bool testbool = false;
+    public bool TestmapData = true;
+    int testNum = 0;
     int currentYPosition = 500;
+    public int resolution = Screen.height;
     void Start()
     {
         ParentCanvas = GetComponent<Canvas>();
@@ -20,7 +26,7 @@ public class MapSelectionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (testbool == true && allMapSelectButtons.Count <= 3)
+        if (testbool == true && allMapSelectButtons.Count < 7)
         {
             AddMapButtonToCanvas();
             testbool = false;
@@ -32,9 +38,46 @@ public class MapSelectionManager : MonoBehaviour
     /// </summary>
     void AddMapButtonToCanvas()
     {
-        Button newButton = Instantiate(MapSelectTemplate, ParentCanvas.transform);
-        newButton.transform.position = new Vector3(currentYPosition, 475, 0);
-        currentYPosition += 550;
-        allMapSelectButtons.Add(newButton);
+        if (allMapSelectButtons.Count <= 2)
+        {
+            Button newButton = Instantiate(MapSelectTemplate, ParentCanvas.transform);
+            ChooseRandomAvailableMap(newButton);
+            newButton.transform.position = new Vector3(currentYPosition, (resolution / 2), 0);
+            currentYPosition += 500;
+            allMapSelectButtons.Add(newButton);
+        }
     }
+
+
+    /// <summary>
+    /// chooses a random map that is not yet in the available mapList
+    /// </summary>
+    /// 
+    //still need to create a temp list that checks what maps are still available*
+    void ChooseRandomAvailableMap(Button newButton)
+    {
+        GameObject emptyTextObject = newButton.transform.Find("Text").gameObject;
+        TMP_Text[] allText = emptyTextObject.GetComponentsInChildren<TMP_Text>();
+        TMP_Text nameOfMapText = allText[0];
+
+
+
+        string randomMap = "feeling null rn";
+        string test = AllAvailableMaps[Random.Range(0, AllAvailableMaps.Count)];
+        if (!AlreadyInstantiatedMaps.Contains(test))
+        {
+            randomMap = test;
+            nameOfMapText.text = randomMap;
+            AlreadyInstantiatedMaps.Add(randomMap);
+        }
+        else if (AlreadyInstantiatedMaps.Count < AllAvailableMaps.Count)
+        {
+            ChooseRandomAvailableMap(newButton);
+        }
+        else
+        {
+            print("we are gonna have a problem here, bud");
+        }
+    }
+
 }
